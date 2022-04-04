d9581e9569d2e3f2dcf7446bb0405e8c8c2f771e
mcditoos <gerardofingurbe@gmail.com>
2022-04-04 08:00:00 +0200

    cuadraticas

--- Mixed/Squeezed/src/krotov/functionals.py
+++ Mixed/Squeezed/src/krotov/functionals.py
@@ -58,9 +58,9 @@ import numpy as np
 import qutip
 
 #from .second_order import _overlap
-from .Integrals import overlap2,coherent_overlap
-#_overlap=overlap2
-_overlap=coherent_overlap
+from .Integrals import  fidelity_sq,overlap2
+_overlap=overlap2
+#_overlap=coherent_overlap
 __all__ = [
     'f_tau',
     'F_ss',
